77004d4bf10ee864c828cce385aa552c586a38be
Martin Laštovica <vonakom@gmail.com>
2024-04-13 19:07:30 +0200

    basic spritework

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -4,6 +4,13 @@ extends CharacterBody2D
 @export var MaxSpeed : int = 500
 @export var Friction : int = 120
 
+@export var direction: Vector2 = Vector2.ZERO
+
+@export var spriteIdle: Texture = null
+@export var spriteRunRight: Texture = null
+
+var sprite2D : Sprite2D = null
+
 enum {IDLE,
 	RUN_UP, RUN_DOWN, RUN_LEFT, RUN_RIGHT,
 	RUN_UP_HOLDING, RUN_DOWN_HOLDING, RUN_LEFT_HOLDING, RUN_RIGHT_HOLDING
@@ -11,24 +18,25 @@ enum {IDLE,
 var state = IDLE
 
 func _ready():
-	set_process(true)
+	sprite2D = find_child('Sprite2D')
 
 func _physics_process(delta):
 	
 	move(delta)
-	print(velocity);
 
 func move(delta):
 	
-	var direction = Input.get_vector('move_left','move_right','move_up',"move_down")	
+	direction = Input.get_vector('move_left','move_right','move_up',"move_down")	
 	
 	if direction == Vector2.ZERO:
-		state = IDLE
+		sprite2D.texture = spriteIdle
 		_apply_friction(Friction*delta)
 	else:
+		sprite2D.texture = spriteRunRight
 		##TODO : make animation switch
 		_apply_movement(direction*Acceleration*delta)
-		
+		_apply_friction(Friction*delta)
+	
 	move_and_slide()
 	
 func _apply_friction(amount):
